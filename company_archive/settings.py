bef2316593410627f9ffca32fed37746dea7d32d
Valerii Sharuda <valerii.sharuda@infoniqa.com>
2024-01-29 23:30:31 +0100

    update

--- company_archive/settings.py
+++ company_archive/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'company_archive.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'documents' / 'templates' / 'documents' ],
+        'DIRS': [BASE_DIR / 'documents' / 'templates' / 'documents/' ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,7 +122,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL =  'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- documents/views.py
+++ documents/views.py
@@ -1,12 +1,17 @@
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Document
+from django.core.paginator import Paginator
 
 class DocumentListView(ListView):
     model = Document
     template_name = 'document_list.html'
     context_object_name = 'documents'
-
+    paginate_by = 10
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        print(context)
+        return context
 class DocumentDetailView(DetailView):
     model = Document
     template_name = 'document_detail.html'
